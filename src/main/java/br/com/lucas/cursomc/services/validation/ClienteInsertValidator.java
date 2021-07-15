d6657d758c73c9f617ab31609c7c88cd46136383
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2021-07-15 18:11:53 -0300

    Validação customizada de CPF ou CNPJ na inserção de cliente

---
+++ src/main/java/br/com/lucas/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,43 @@
+package br.com.lucas.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import br.com.lucas.cursomc.domain.enums.TipoCliente;
+import br.com.lucas.cursomc.dto.ClienteNewDTO;
+import br.com.lucas.cursomc.resources.exceptions.FieldMessage;
+import br.com.lucas.cursomc.services.validation.utils.BR;
+
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+	
+ 	@Override
+ 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+ 		
+ 		List<FieldMessage> list = new ArrayList<>();
+ 		
+ 		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+ 			list.add(new FieldMessage("cpfOuCnpj", "O CPF é inválido!"));
+ 		}
+ 		
+ 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+ 			list.add(new FieldMessage("cpfOuCnpj", "O CNPJ é inválido!"));
+ 		}
+
+ // inclua os testes aqui, inserindo erros na lista
+
+ 		for (FieldMessage e : list) {
+ 			context.disableDefaultConstraintViolation();
+ 			context.buildConstraintViolationWithTemplate(e.getMessage())
+ 			.addPropertyNode(e.getFieldName()).addConstraintViolation();
+ 		}
+ 		return list.isEmpty();
+ 	}
+}
